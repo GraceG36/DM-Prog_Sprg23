73604f2b83b7f9baeeb89b645b352be14f31357b
GraceG36 <116042587+GraceG36@users.noreply.github.com>
2023-02-03 23:53:17 -0600

    Project 3

--- app.js
+++ app.js
@@ -1,34 +1,27 @@
 //Created by Grace Garrand
-//1-27-2023
+//2-3-2023 (Yay...my bday!)
 
-let life = 2450; //life is now a number
-life = life - 350;//this changes life value to -350
 
-const bag = 7; //GLOBAL VARIABLES
-const e = 5; //GLOBAL VARIABLES
-const x = 28; //GLOBAL VARIABLES
-const w = 3; //GLOBAL VARIABLES
-
-let = 4; //undefined
-
-let equa = e + x - w; //Mathematical operations
-
-{
-    let key = 2;//variable in block that can NOT be used outside
-    let hole = 8;
-    var lock = key + hole;//this variable can be used outside of block
-
-    const dev = "Four score and";
-    const elope = " seven years ago"; 
-    var speech = dev + elope; //String & block variable(can be used outside of block)
+function changeImage() {
+    let displayImage = document.getElementById('image1')
+    if(displayImage.src.match('images/Girl.jpg')) {
+        displayImage.src = 'images/Disintagrated_Girl.png'
+    } else {
+        displayImage.src = 'images/Girl.jpg'
+    }
+}
+function msgText() {
+    alert("Click on the picture to see the girl start to disinagrate!")
 }
 
-const ring = {metal:"Gold", stone:"Opal", color:"White"}
 
-alert("Rings can be made with " + ring.metal + " and set with " + ring.color +" " +ring.stone + "s")
-// ^ is an alert with a STRING using OBJECT properties
+const girl = {hairType:'curly',
+ shirtType:'striped',
+ pantType:'jeans' ,
+ nailColor:'black',
+ fullDescript: function() { 
+    return this.hairType + " " + this.shirtType;
+}};
 
-console.log(life, equa, bag, "*changing variable, Mathematical sum, global unchanging variable");
-console.log(speech, "*string");
-console.log(ring, "*object")
-console.log(lock, "*from block")
+fullDescript()
+msgText()
