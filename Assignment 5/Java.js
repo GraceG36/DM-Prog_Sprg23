23ef6fd7b8c4640e3f5d64dc3d6b30f2dfca27e7
GraceG36 <116042587+GraceG36@users.noreply.github.com>
2023-02-18 01:22:39 -0600

    Update1.3

--- Assignment 5/Java.js
+++ Assignment 5/Java.js
@@ -1,15 +1,30 @@
+
+// Changing the look w/ HTML Dom
+
 const text = document.querySelector('.changeP');
 const change12 = document.querySelector('.change12');
-const addList = document.querySelector(.addList)
-
-const bedTime = document.querySelectorAll('.bedTime li');
-
 change12.addEventListener('click', function(){
     text.classList.toggle('change1')
 })
 
+//Change color button and const
+
+const bedTime = document.querySelectorAll('.bedTime li');
 for(user of bedTime){
     user.addEventListener('click', function(){
         this.style.color = 'purple';
+        
     })
 }
+
+//Adding additional list items from user input
+const addList = document.querySelector('.addList');
+const additionalLi = document.querySelector('.additionalLi');
+const userList = document.querySelector('.bedTime')
+
+additionalLi.addEventListener('click', function(){
+    const addLi = document.createElement('LI');
+    const liContent = document.createTextNode(addList.value);
+    addLi.appendChild(liContent);
+    userList.appendChild(addLi);
+});
