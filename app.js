f83c44e672afd8ba55405043c8ce23b117d4dcd1
GraceG36 <116042587+GraceG36@users.noreply.github.com>
2023-02-25 13:32:13 -0600

    Just Needs tweaking

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const pic1 = {hands:"holding", pot:"forming the rim", tool:"rib"};
+const pic1Contents = {
+    hands:"forming the rim",
+    pot:"being shaped",
+    tool:"rib",
+    material: ["clay", "water", "love"]
+};
 
-document.getElementById("discript").innerHTML = pic1 ;
+
+document.getElementById("test").innerHTML = (pic1Contents.material);
+
+const tools = ["Rib", "Wire tool", "Clay", "Bucket","Wheel"]
+
+document.getElementById("tools").innerHTML = tools ;
+
+document.getElementById("singleTool").innerHTML = tools[2];
+
+tools.push("Cottage Cheese. Check this out, I added an item! ");
+
+document.getElementById("addedItem").innerHTML = tools ;
