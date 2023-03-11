c0aa7d37ade61d7016fde8bd102b90687c134596
GraceG36 <116042587+GraceG36@users.noreply.github.com>
2023-03-10 22:27:50 -0600

    Java Game version 1
    Java Game version 1

--- app.js
+++ app.js
@@ -1,19 +1,67 @@
-const pic1Contents = {
-    hands:"forming the rim",
-    pot:"being shaped",
-    tool:"rib",
-    material: ["clay", "water", "love"]
-};
+const game = ()=> {
+    let pScore = 0;
+    let cScore = 0;
+
+    const startGame = ()=> {
+        const playBtn = document.querySelector('.intro button');
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('.match');
+
+        playBtn.addEventListener('click', () => {
+            introScreen.classList.add('fadeOut');
+            match.classList.add('fadeIn');
+         });
+    };
+
+    const playMatch = () => {
+        const options = document.querySelectorAll('.options button');
+        const playerHand = document.querySelector('.player-hand');
+        const computerHand = document.querySelector('.computer-hand');
+        //CP options
+        const computerOptions = ['rock', 'paper', 'scissors'];
+
+        options.forEach(option =>{
+            option.addEventListener('click', function() {
+                //CP Choice
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+
+                //Update Images
+                playerHand.src = `./assets/${this.textContent}.png`;
+                computerHand.src = `./assets/${computerChoice}.png`;
+            });
+            
+        });
+    };
+
+    const compareHands = (playerChoice, computerChoice) =>{
+        const winner = document.querySelector('.winner');
+        if(playerChoice === computerChoice){
+            winner.textContent = 'It is a tie';
+            return;
+        }
+    
+        //check for rock
+        if(playerChoice === 'rock'){
+            if(computerChoice === 'scissors'){
+                winner.textContent = 'Player Wins'
+                return;
+            }else{
+                winner.textContent = 'Computer Wins';
+                return;
+            }
+        }
+      
+
+    }
+    //Check for Paper
+
+        
+    };
+
+    startGame();
+    updateScore();
+    playMatch();
 
 
-document.getElementById("test").innerHTML = (pic1Contents.material);
-
-const tools = ["Rib", "Wire tool", "Clay", "Bucket","Wheel"]
-
-document.getElementById("tools").innerHTML = tools ;
-
-document.getElementById("singleTool").innerHTML = tools[2];
-
-tools.push("Cottage Cheese. Check this out, I added an item! ");
-
-document.getElementById("addedItem").innerHTML = tools ;
+game();
